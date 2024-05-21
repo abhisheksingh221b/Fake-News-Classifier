b41b753b6bd4769c4f4b3d9ab6dec05d65851fe8
abhisheksingh221b <abhisheksingh221b@gmail.com>
2024-05-21 17:17:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def fake_news(news):
     return prediction
 
 def main():
-    st.title('Fake News Classification App')
+    st.title('Fake News Classifier')
     st.subheader("Input the news content below")
     sentence = st.text_area("", height=200)
     predict_btn = st.button("Predict")
