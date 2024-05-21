aea38abfa7184818673409e93711657c795b0595
abhisheksingh221b <abhisheksingh221b@gmail.com>
2024-05-21 16:15:47 +0530

    Update FN Web App.py

--- FN Web App.py
+++ FN Web App.py
@@ -7,7 +7,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from PIL import Image
-import os
 
 nltk.download('punkt')
 nltk.download('stopwords')
